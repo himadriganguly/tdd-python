fd10a52e675edcb71c5e0eadd193963d1a4c999f
Himadri Ganguly <himadri@colourdrift.com>
2016-03-22 03:58:43 +0530

    Added new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+	
 from lists.models import Item
 
 # Create your views here.
@@ -8,7 +8,7 @@ def home_page(request):
 	if request.method == 'POST':
 		new_item_text = request.POST['item_text']
 		Item.objects.create(text=new_item_text)
-		return redirect('/')
+		return redirect('/lists/the-only-list-in-the-world/')
 	# else:
 		# new_item_text = ''
 		
@@ -16,5 +16,9 @@ def home_page(request):
 		# 'new_item_text': new_item_text,
 	# })
 	
+	# items = Item.objects.all()
+	return render(request, 'home.html')
+
+def view_list(request):
 	items = Item.objects.all()
-	return render(request, 'home.html', {'items': items})
+	return render(request, 'list.html', {'items': items})
